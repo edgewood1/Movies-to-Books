8a4b5b12a867690457f5fcc51a3c087cb753f382
Katie <katiearina@gmail.com>
2017-04-04 18:46:53 -0400

    Added Firebase link/initialization

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyBWFWAg2YMXu1TO-RUD2APOpX_Knds8m4o",
+		authDomain: "movies-to-books-c3182.firebaseapp.com",
+		databaseURL: "https://movies-to-books-c3182.firebaseio.com",
+		projectId: "movies-to-books-c3182",
+		storageBucket: "movies-to-books-c3182.appspot.com",
+		messagingSenderId: "28649429385"
+	};
+	firebase.initializeApp(config);

171307b03d6af171e1ece8cc5644493295b8e2fe
Mary Morvan <mamorvan@yahoo.com>
2017-04-04 20:00:54 -0400

    change bookcalls.js

--- assets/javascript/bookcalls.js
+++ assets/javascript/bookcalls.js
@@ -1,6 +1,6 @@
-var subject = "action";
+var subject = "juvenile,fiction";
 
-var queryURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + subject + "&printTYpe=books&langRestrict=en&maxResults=40&key=AIzaSyDLWrPgW350LzRa-B-z83xg5uKzAjROB1I";
+var queryURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + subject + "&printType=books&langRestrict=en&maxResults=40&key=AIzaSyDLWrPgW350LzRa-B-z83xg5uKzAjROB1I";
 
 // Creating an AJAX call for the specific movie button being clicked
 $.ajax({
@@ -13,79 +13,15 @@ $.ajax({
   //get categories only
   for (var i =0; i < 40; i++) {
     console.log(response.items[i].volumeInfo.categories);
+    console.log(response.items[i].volumeInfo.title);
+    console.log(response.items[i].volumeInfo.authors);
+    console.log(response.items[i].volumeInfo.description);
+    console.log(response.items[i].volumeInfo.pageCount);
+    console.log(response.items[i].volumeInfo.imageLinks.thumbnail);
+    console.log(response.items[i].volumeInfo.previewLink);
   }
   
-
-
-  for (var i = 0; i < 10; i++) {
-      var bookId = response.items[i].id;
-      console.log(bookId);
-
-      var queryByIdURL = "https://www.googleapis.com/books/v1/volumes/"+ bookId;
-
-        
-        $.ajax({
-          url: queryByIdURL,
-          method: "GET"
-        }).done(function(responseById) {
-        
-          console.log(responseById);
-          console.log(responseById.volumeInfo.title);
-          console.log(responseById.volumeInfo.authors);
-          console.log(responseById.volumeInfo.description);
-          console.log(responseById.volumeInfo.categories);
-          console.log(responseById.volumeInfo.maturityRating);
-          console.log("average rating: " + responseById.volumeInfo.averageRating);
-          console.log("number of ratings: " + responseById.volumeInfo.ratingsCount);
-      });
-
-  };
-
-  for (var i = 10; i < 20; i++) {
-      var bookId = response.items[i].id;
-      console.log(bookId);
-
-      var queryByIdURL = "https://www.googleapis.com/books/v1/volumes/"+ bookId;
-
-        
-        $.ajax({
-          url: queryByIdURL,
-          method: "GET"
-        }).done(function(responseById) {
-        
-          console.log(responseById);
-          console.log(responseById.volumeInfo.title);
-          console.log(responseById.volumeInfo.authors);
-          console.log(responseById.volumeInfo.description);
-          console.log(responseById.volumeInfo.categories);
-          console.log(responseById.volumeInfo.maturityRating);
-          console.log("average rating: " + responseById.volumeInfo.averageRating);
-          console.log("number of ratings: " + responseById.volumeInfo.ratingsCount);
-      });
-  };
-
-  for (var i = 20; i < 30; i++) {
-      var bookId = response.items[i].id;
-      console.log(bookId);
-
-      var queryByIdURL = "https://www.googleapis.com/books/v1/volumes/"+ bookId;
-
-        
-        $.ajax({
-          url: queryByIdURL,
-          method: "GET"
-        }).done(function(responseById) {
-        
-          console.log(responseById);
-          console.log(responseById.volumeInfo.title);
-          console.log(responseById.volumeInfo.authors);
-          console.log(responseById.volumeInfo.description);
-          console.log(responseById.volumeInfo.categories);
-          console.log(responseById.volumeInfo.maturityRating);
-          console.log("average rating: " + responseById.volumeInfo.averageRating);
-          console.log("number of ratings: " + responseById.volumeInfo.ratingsCount);
-      });
-  };
+ 
   
 });
 
